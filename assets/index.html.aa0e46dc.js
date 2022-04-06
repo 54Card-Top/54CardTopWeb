4f49405f1f6bb02247b57e874d4d16511183386b
54cardtop <54card.top@protonmail.com>
2022-04-06 20:45:40 +0800

    deploy web : V0.02:update

---
+++ assets/index.html.aa0e46dc.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app.1577efb5.js";const e={};function _(n,t){return null}var a=r(e,[["render",_]]);export{a as default};
---
+++ assets/welcome.html.1d3e8e58.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to 54Card.Top",lang:"en-US",frontmatter:{index:1,title:"Welcome to 54Card.Top",author:"54Card.Top",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`54Card Top : Theme poker sets.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://54card.top/welcome.html"}],["meta",{property:"og:title",content:"Welcome to 54Card.Top"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"54Card.Top"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>54Card Top : Theme poker sets.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"54Card Top",slug:"_54card-top",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why 54CardTop?",slug:"why-54cardtop",children:[]},{level:2,title:"Tech Stack to Build 54Card Top",slug:"tech-stack-to-build-54card-top",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:164924888e4,updatedTime:164924888e4,contributors:[{name:"54cardtop",email:"54card.top@protonmail.com",commits:1}]},readingTime:{minutes:.44,words:133},filePathRelative:"welcome.md"};export{e as data};
